b3757246fbe18a9367fad28d4cec5ca1c7f92243
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-05-23 21:30:09 +0300

    ex01

--- ex1.py
+++ ex1.py
@@ -1,1 +1,7 @@
-print('sdsd')
+number = int(input('введите день недели '))
+if number == 6 or number == 7:
+    print("Weekend")
+elif 1 <= number <= 5:
+    print("Weekday")
+else:
+    print("wrong number")
