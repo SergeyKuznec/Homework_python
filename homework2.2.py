988c782cf383970e9c246e9665c30c4ee79f0c80
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-05-26 18:50:13 +0300

    2.2

--- homework2.2.py
+++ homework2.2.py
@@ -1,7 +1,9 @@
 n = int(input('введите число n '))
 sum = 0
+diction = {}
 for i in range(1, n+1):
     sum += (1+1/i)**i
-    print((1+1/i)**i, end = ' ')
-print()
+    diction[i] = (1+1/i)**i
+    ##print((1+1/i)**i, end = ' ')
+print(diction)
 print('сумма равна ', sum)
